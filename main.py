f9cbc8252414ab3258d71bb0bf965dab257cb902
Zoe <zoedroulias@gmail.com>
2024-08-08 18:39:37 -0400

    change var names

--- main.py
+++ main.py
@@ -8,17 +8,9 @@ def home():
 
 @app.route("/generate-pattern", methods=["POST"])
 def generatePatterns():
-    color1 = "red"
-    color2 = "blue"
-    color3 = "white"
-    
-    print(f"{color1}\n{color2}\n{color3}")
-    
-    colors = {
-        "color 1": color1,
-        "color 2": color2,
-        "color 3": color3
-    }
+    updated_color1 = request.form.get('color1', 'red')
+    updated_color2 = request.form.get('color2', 'green')
+    updated_color3 = request.form.get('color3', 'blue')
     
     return render_template("results.html")
 
