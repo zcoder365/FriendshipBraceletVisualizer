f63a14ddab3fa8457ed6046e63271e3566e7e8eb
Zoe <zoedroulias@gmail.com>
2024-08-08 18:15:57 -0400

    get the colors from the form and print them

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def home():
 
 @app.route("/generate-pattern", methods=["POST"])
 def generatePatterns():
+    color1 = request.form["color1"]
+    color2 = request.form["color2"]
+    color3 = request.form["color3"]
+    
+    print(f"{color1}\n{color2}\n{color3}")
+    
     return render_template("results.html")
 
 app.run(debug=True)
