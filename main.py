68ea952764f774643db4705dd131092b3c8c765f
Zoe <zoedroulias@gmail.com>
2024-08-08 15:55:56 -0400

    add a new route for showing the results

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/generate-patterns")
+def generatePatterns():
+    return render_template("results")
+
 app.run(debug=True)
